5b4f333db798b0efc3b02f6a5b983672d6423436
Junya Kemmochi <github@tsurugeek.com>
2018-07-16 15:45:19 +0900

    add test case

--- spec/eratos_spec.rb
+++ spec/eratos_spec.rb
@@ -2,6 +2,7 @@
 
 describe "eratos.rb" do
   example "素数が表示されること" do
+    expect(`ruby eratos.rb 30`).to eq "2,3,5,7,11,13,17,19,23,29\n"
     expect(`ruby eratos.rb 120`).to eq "2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113\n"
   end
 
